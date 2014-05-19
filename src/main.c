e9882045b29443602f3be21fd3f31137f34c1f18
Ali Doost-Hosseini <ali.d.hosseini@gmail.com>
2014-05-19 15:27:47 +0430

    Main.c

--- src/main.c
+++ src/main.c
@@ -1,20 +1,40 @@
 #include < HOS.h >
 
-
 unsigned char *memcpy(unsigned char *dest, const unsigned char *src, int count)
 {
+    int i = 0;
+start_cpy:
+    *(dest+i) = *(src+i);
+    i = i + 1;
+    if (i < count)
+       goto start_cpy;
+    return dest;
     /* Add code here to copy 'count' bytes of data from 'src' to
     *  'dest', finally return 'dest' */
 }
 
 unsigned char *memset(unsigned char *dest, unsigned char val, int count)
 {
+    int i = 0;
+start_set:
+    *(dest+i) = val;
+    i = i + 1;
+    if (i < count)
+	goto start_set;
+    return;
     /* Add code here to set 'count' bytes in 'dest' to 'val'.
     *  Again, return 'dest' */
 }
 
 unsigned short *memsetw(unsigned short *dest, unsigned short val, int count)
 {
+    int i = 0;
+start_set:
+    dest[i] = val;
+    i = i + 1;
+    if (i < count)
+	goto start_set;
+    return dest;
     /* Same as above, but this time, we're working with a 16-bit
     *  'val' and dest pointer. Your code can be an exact copy of
     *  the above, provided that your local variables if any, are
@@ -23,6 +43,9 @@ unsigned short *memsetw(unsigned short *dest, unsigned short val, int count)
 
 int strlen(const char *str)
 {
+    int i = 0;
+    for (; str[i] != 0; i = i+1);
+    return i;
     /* This loops through character array 'str', returning how
     *  many characters it needs to check before it finds a 0.
     *  In simple words, it returns the length in bytes of a string */
@@ -52,7 +75,6 @@ void outportb (unsigned short _port, unsigned char _data)
 void main()
 {
     /* You would add commands after here */
-
     /* ...and leave this loop in. There is an endless loop in
     *  'start.asm' also, if you accidentally delete this next line */
     for (;;);
